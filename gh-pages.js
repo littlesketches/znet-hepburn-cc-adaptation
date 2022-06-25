d863ec2d43997d77f8b955973f9d21ed2f244a39
littlesketches <brendan@littlsketch.es>
2022-06-26 00:28:37 +1000

    update deployment

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'https://github.com/littlesketches/znet-hepburn-cc-adaptation', // Update to point to your repository
+		repo: 'https://github.com/littlesketches/znet-hepburn-cc-adaptation.git', // Update to point to your repository
 		user: {
 			name: 'littlesketches', // update to use your name
 			email: 'brendan@littlesketch.es' // Update to use your email
